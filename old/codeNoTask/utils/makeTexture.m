17fcc56a238d236089083ec9f3e40792801f76d1
Alex L. White <alexlw@uw.edu>
2017-12-11 17:06:35 -0800

    removed "old" folder, not necessary

--- old/codeNoTask/utils/makeTexture.m
+++
@@ -1,8 +0,0 @@
-function tex = makeTexture(display,img)
-
-if isfield(display,'gamma')
-    img =  display.gamma(ceil(img)+1);
-end
-tex=Screen('MakeTexture', display.windowPtr, img);
-
-
--- old/codeNoTask/utils/myCloseExp.m
+++
@@ -1,6 +0,0 @@
-function myCloseExp
-% close ptb and eyelink
-Eyelink('Shutdown');
-ShowCursor;
-Screen('CloseAll');
-commandwindow
